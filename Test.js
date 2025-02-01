59d965bcfd4ec4f621ab069b9bac3ebc770a14aa
HornR93 <hornr93@gmail.com>
2025-02-01 09:11:20 +0100

    čtvrtek

---
+++ Test.js
@@ -0,0 +1,75 @@
+
+1
+console.log('text')
+
+2
+let - vytahuje určuje údaje (měnitelné)
+var - asi stejné?
+const - konstantní neměnné
+
+3
+nestane se nic - hodnota zůstane stejná
+
+4
+Ahoj světe
+
+5
+nastane chyba? "5" - string a 3 je číslo
+
+6
+pomocí funkce return?
+
+7
+????????????????????????????
+function()
+
+8
+console.log - zapisuje do konzole, return vrací proměnnou
+
+9
+15
+
+10
+vůbec nevím????????????????????????????
+vyměním znaménka v těle funkce
+
+11
+jako ve wordu - Pokud, nebo
+
+
+12
+zápis do konzole "1 je menší než 2"
+
+13
+Switch mění dle námy určených pravidel
+If-else hledá pravdu/nepravdu
+
+14
+k ukončení daného pravidla / případu (case)
+
+15
+switch (p) {
+    case 1:
+        console.log('Pondělí');
+        break;
+    case 2:
+        console.log('Úterý');
+        break;
+
+    default:
+        console.log('Jiný den');
+}
+
+16
+Objekt je jen jeden konkrétní, pole je více hodnot
+
+17
+je to pozice č. 2 (začínáme od 0)
+
+18
+jmena.[1]='Jackie'
+
+19
+Element.person[0].věk
+
+20
