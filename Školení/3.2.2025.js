83619a0f3a3aa365afd9b9bea092ff630e94c500
HornR93 <hornr93@gmail.com>
2025-02-09 09:54:46 +0100

    hodně

---
+++ Školení/3.2.2025.js
@@ -0,0 +1,22 @@
+const jmeno = document.getElementById("jmeno")
+const error = document.getElementById("error")
+
+jmeno.addEventListener("keyup", (e)) => {
+
+
+    const idHlasky = e.target.dataset.error
+
+
+
+}
+
+if (e.target.value.length <4){
+
+    document.getElementById(Error).style.display = "inline"; 
+
+}
+else {
+
+    document.getElementById(Error).style.display = "none"
+
+}
